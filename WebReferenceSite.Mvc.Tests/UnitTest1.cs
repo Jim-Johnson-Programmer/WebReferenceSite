d612b5366133cadfbe5b49a2ff8f31b43bbdc858
Jim <email_programmer_jim@yahoo.com>
2023-01-04 23:19:33 -0600

    Add project files.

---
+++ WebReferenceSite.Mvc.Tests/UnitTest1.cs
@@ -0,0 +1,14 @@
+using System;
+using Xunit;
+
+namespace WebReferenceSite.Mvc.Tests
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
---
+++ WebReferenceSite/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace WebReferenceSite.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
